f424f9ebd83bf08546cb90c3456a6e08d2ef270f
Joao Hentz <joaowitormiiller@gmail.com>
2025-10-23 01:35:42 -0300

    adjust

---
+++ Core/Inc/definitions.h
@@ -0,0 +1,15 @@
+/*
+ * definitions.h
+ *
+ *  Created on: Oct 23, 2025
+ *      Author: joaow
+ */
+
+#ifndef INC_DEFINITIONS_H_
+#define INC_DEFINITIONS_H_
+
+#define DEADTIME_NS 50
+
+
+
+#endif /* INC_DEFINITIONS_H_ */
